88a9defaca19d3cd0bf667c258732369e84c3313
Ben Morse <ben.morse@palecoyote.dev>
2026-04-24 20:43:23 -0700

    kill FILES_PATH

--- src/constants/files.ts
+++ src/constants/files.ts
@@ -4,12 +4,3 @@
  * Development: https://cf87fed332615a4dce68de5984a48f49.balena-devices.com
  */
 export const BASE_WEB_DAV_URL = '';
-
-/**
- * Files Path
- */
-export const FILES_PATH = {
-  media: '/media',
-  weights: '/weights',
-  datasets: '/datasets',
-};
