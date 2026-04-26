0512dc40fcf0791f9ce0eabfc09b4ac44e26c7ad
Ben Morse <ben.morse@palecoyote.dev>
2026-04-25 18:46:59 -0700

    at least add *one* test.  more later

--- pkg/plugin/datasource_test.go
+++ pkg/plugin/datasource_test.go
@@ -7,6 +7,23 @@ import (
 	// "github.com/grafana/grafana-plugin-sdk-go/backend"
 )
 
+func TestConnectionStrategy(t *testing.T) {
+	_, err := makeConnectionStrategy(&InstanceSettings{ConnectionType:"AUTO"})
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = makeConnectionStrategy(&InstanceSettings{ConnectionType:"INFO"})
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = makeConnectionStrategy(&InstanceSettings{ConnectionType:"CORNDOG"})
+	if err == nil {
+		t.Error("makeConnectionStrategy isn't erroring on a weird ConnectionType")
+	}
+}
+
 func TestQueryData(t *testing.T) {
 	// @TODO: figure out how to mock jupyter 
 	// ds := Datasource{}
