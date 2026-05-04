1ea5812287a64ad968943bff90e95c677e7f7564
Ben Morse <ben.morse@palecoyote.dev>
2026-05-04 10:44:28 -0700

    filter queries without UUID

--- src/datasource.ts
+++ src/datasource.ts
@@ -3,8 +3,6 @@ import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 
 import { KernelSpec, KernelSpecResponse, MyQuery, MyDataSourceOptions, DEFAULT_QUERY, PathEntry } from './types';
 
-import { v4 as uuidv4 } from 'uuid';
-
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   options: MyDataSourceOptions;
 
@@ -14,7 +12,7 @@ export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptio
   }
 
   getDefaultQuery(_: CoreApp): Partial<MyQuery> {
-    return { ...DEFAULT_QUERY, uuid: uuidv4() };
+    return DEFAULT_QUERY;
   }
 
   applyTemplateVariables(query: MyQuery, scopedVars: ScopedVars) {
@@ -25,7 +23,7 @@ export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptio
 
   filterQuery(query: MyQuery): boolean {
     // if no query has been provided, prevent the query from being executed
-    return true;
+    return query.uuid !== undefined;
   }
 
   getNotebooks(): Promise<PathEntry[]> {
