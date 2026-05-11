0f25307db00f7dcccfea4db88677751e6380654b
Ben Morse <ben.morse@palecoyote.dev>
2026-05-10 18:53:51 -0700

    an actual test using mocks!!

--- pkg/plugin/datasource_test.go
+++ pkg/plugin/datasource_test.go
@@ -1,9 +1,13 @@
 package plugin
 
 import (
+	"context"
 	"testing"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/stretchr/testify/assert"
+	"github.com/Theia-Scientific/jupyter-datasource/pkg/jupyterclient_test"
+	"github.com/Theia-Scientific/jupyter-datasource/pkg/jupyterclient"
 )
 
 func TestConnectionStrategy(t *testing.T) {
@@ -17,3 +21,22 @@ func TestConnectionStrategy(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+func TestCallResource(t *testing.T) {
+	httpClient := jupyterclient_test.NewMockIJupyterHttpClient(t)
+	httpClient.EXPECT().GetListing("some/path").Return([]jupyterclient.PathEntry{}, nil);
+	d := &Datasource{
+		sessions: make(map[string]SessionState),
+		createdKernels: []string{},
+		httpClient: httpClient,
+		context: context.Background(),
+		cancel: func(){},
+	}
+
+	rv, err := d.callResource(&backend.CallResourceRequest{
+		Path: "list",
+		URL: "http://whatever/list?path=some/path",
+	});
+
+	assert.Equal(t, string(rv), "[]")
+	assert.Nil(t, err)
+}
