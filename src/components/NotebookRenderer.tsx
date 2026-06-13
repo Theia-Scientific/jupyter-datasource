9c6f7cd86eeaff38b986ecc9da92f96505ab859c
Ben Morse <ben.morse@palecoyote.dev>
2026-06-12 20:10:38 -0700

    use grafana theme (dark/light anyway) to style syntax highlighter

--- src/components/NotebookRenderer.tsx
+++ src/components/NotebookRenderer.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark as dark, materialLight as light } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Markdown from 'react-markdown';
 import { Notebook } from '@theia/types';
+import { useTheme2 } from '@grafana/ui';
 
 interface Props {
   notebook: Notebook;
 }
 
 export function NotebookRenderer({notebook}: Props) {
+  const theme = useTheme2();
+  const style = theme.isLight ? light : dark;
+
   const cells = notebook.content.cells.map((cell, idx) => (
     cell.cell_type === 'code' ? (
-      <SyntaxHighlighter language={'python'} key={idx}>
+      <SyntaxHighlighter
+        language={'python'}
+        key={idx}
+        style={style}
+        showLineNumbers={true}>
         {cell.source}
       </SyntaxHighlighter>
     ) : (
