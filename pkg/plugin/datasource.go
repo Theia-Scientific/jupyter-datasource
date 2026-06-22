da9ce968c5a1ee70bebb9ffe9d395c67670fa406
Ben Morse <ben.morse@palecoyote.dev>
2026-06-22 08:49:31 -0700

    Add bool support to GF_VARS (#75)
    bool vars will evaluate to False if they're in the list:
      ["false", "f", "no", "n", "off", "0", "disable", "disabled", ""]
    and True otherwise.  The list is stored in the global variable
    GF_FALSES so users can override it in their notebooks if they
    want to.

--- pkg/plugin/datasource.go
+++ pkg/plugin/datasource.go
@@ -452,13 +452,17 @@ func (d *Datasource) query(pctx context.Context, query backend.DataQuery) backen
 
 	// got a session now
 	var qb strings.Builder
-	qb.WriteString(`class GrafanaSupport(dict):
+	qb.WriteString(`
+GF_FALSES = ["false", "f", "no", "n", "off", "0", "disable", "disabled", ""]
+class GrafanaSupport(dict):
   def float(self, name, default=0.0):
     return float(self[name]) if name in self and self[name] != "" else default
   def int(self, name, default=0):
     return int(self[name]) if name in self and self[name] != "" else default
   def str(self, name, default=""):
     return self[name] if name in self and self[name] != "" else default
+  def bool(self, name, default=False):
+    return (self[name].lower() not in GF_FALSES) if name in self and self[name] != "" else default
   def list(self, name, default=[]):
     return self[name].replace("{", "").replace("}", "").split(",") if name in self and self[name] != "" and self[name] != "{}" else default
 GF_VARS = GrafanaSupport({
