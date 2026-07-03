9ba1fb8f0b287af3c5fe96bfb2c4c4d62a62df26
Ben Morse <ben.morse@palecoyote.dev>
2026-07-03 09:14:40 -0700

    rename to theia-jupyter-datasource to match org slug

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,14 @@
 import i18next from 'i18next';
-import en from './locales/en-US/theiascientific-jupyter-datasource.json';
+import en from './locales/en-US/theia-jupyter-datasource.json';
 
 i18next
   .init({
     returnEmptyString: false, // allows empty string as valid translation
     // lng: 'en-US', // or add a language detector to detect the preferred language of your user
     fallbackLng: 'en-US',
-    defaultNS: 'theiascientific-jupyter-datasource',
+    defaultNS: 'theia-jupyter-datasource',
     resources: {
-      'en-US': { 'theiascientific-jupyter-datasource': en },
+      'en-US': { 'theia-jupyter-datasource': en },
     },
   })
 
