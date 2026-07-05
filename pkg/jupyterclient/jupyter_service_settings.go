5635f3e714617f68b70f0211b5e1fc561842a456
Ben Morse <ben.morse@palecoyote.dev>
2026-07-05 10:34:44 -0700

    remove os.Getenvs

--- pkg/jupyterclient/jupyter_service_settings.go
+++ pkg/jupyterclient/jupyter_service_settings.go
@@ -1,10 +1,5 @@
 package jupyterclient
 
-import (
-	"fmt"
-	"os"
-)
-
 type JupyterServiceSettings struct {
 	BaseUrl string
 	Token   string
@@ -13,11 +8,4 @@ type JupyterServiceSettings struct {
 type JupyterHttpClient struct {
 	BasePath   string
 	AuthHeader string
-}
-
-func DefaultJupyterServiceSettings() (*JupyterServiceSettings, error) {
-	return &JupyterServiceSettings{
-		BaseUrl: fmt.Sprintf("http://%s:%s", os.Getenv("JUPYTER_SERVICE_HOST"), os.Getenv("JUPYTER_SERVICE_PORT")),
-		Token:   os.Getenv("JUPYTER_TOKEN"),
-	}, nil
 }
